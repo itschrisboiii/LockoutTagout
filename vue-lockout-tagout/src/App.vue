ee9edf3d162f3140eda2ebc48278506c0574ff69
itschrisboiii <cloya100@gmail.com>
2024-04-14 02:37:31 -0700

    started some compoents

---
+++ vue-lockout-tagout/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <LockoutVue />
+  <TagoutVue />
+</template>
+
+<script>
+import LockoutVue from './components/icons/Lockout.vue'
+import TagoutVue from './components/icons/Tagout.vue'
+export default {
+  components: {
+    LockoutVue,
+    TagoutVue
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-lockout-tagout/src/components/icons/Lockout.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1>Lockout</h1>
+    <form  @submit.prevent="submitForm" class="lockout">
+        <label for="equipment">Select Equipment:</label>
+      <select v-model="equipment" id="equipment">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      <label for="associate">Select Associate:</label>
+      <select v-model="associate" id="associate">
+        <option value="option1">Option A</option>
+        <option value="option2">Option B</option>
+        <option value="option3">Option C</option>
+      </select>
+      <label for="time">Select Time:</label>
+      <input type="time" v-model="time" id="time">
+      <button type="submit" class="submit-btn">Submit</button>
+    </form>
+  </template>
+
+<script>
+export default {
+    data() {
+        return {
+            equipment: 'option1', // Default selected option for select1
+            associate: 'option1', // Default selected option for select2
+            time: ''
+        } 
+    },
+    methods: {
+        submitForm() {
+        console.log("Lockout submitted");
+        console.log("Selected Equipment:", this.equipment);
+        console.log("Selected Associate:", this.associate);
+        console.log("Selected Time:", this.time);
+      }
+    }
+}
+</script>
+
+<style scoped>
+    .lockout {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    h1 {
+        text-align: center;
+    }
+</style>
